8047731974bf5dd777e5405ad9fb722f24669663
Wahyu Amirulloh <amirullohw@gmail.com>
2021-08-09 13:37:40 +0700

    Project Boilerplate completed

---
+++ webpack.common.js
@@ -0,0 +1,44 @@
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const copyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+	entry: "./src/app.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css/,
+				use: [
+					{
+						loader: "style-loader"
+					},
+					{
+						loader: "css-loader"
+					}
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource',
+			},
+		]
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			template: "./src/index.html",
+			filename: "index.html"
+		}),
+		new copyWebpackPlugin({
+			patterns: [
+				{
+					from: "./src/images/",
+					to: "./images/"
+				}
+			]
+		})
+	]
+}
